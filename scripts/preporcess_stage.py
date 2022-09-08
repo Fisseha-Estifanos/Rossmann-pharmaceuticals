1173a10b6c2131db438393aa53f63c5a4a4206a1
fish <fisseha.137@gmail.com>
2022-09-08 15:58:45 +0300

    added the preprocessing script

---
+++ scripts/preporcess_stage.py
@@ -0,0 +1,8 @@
+"""
+The data preprocessing stage script.
+"""
+
+print('Starting the data preprocessing stage . . .')
+print('This is the data preprocessing stage . . .')
+print('This stage has been completed in the data preparation stage of the pipeline . . .')
+print('Data preprocessing stage completed. . .')
